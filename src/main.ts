e55ea83aa48f8a149fc3e799e42a81c2bd73e88f
Ángela Real <angelarealr@gmail.com>
2024-10-23 17:02:36 +0200

    comenzando el ejercicio

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,58 @@
-import "./style.css";
+// Interface
+interface GrupoMusical {
+    nombre: string;
+    añoFundacion: number;
+    activo: boolean;
+    genero: string;
+};
 
-console.log("Hello Typescript!");
+// Estilos de música
+const popRock = "🎵 Pop Rock";
+const rock = "🎸 Rock";
+const hardRock = "🤘 Hard Rock";
+const clasica = "🎼 Clásica";
+
+// Estilos de texto
+const estiloNombre = "font-weight: 900; font-size: 18px; color: green;"
+
+// Grupos musicales
+const grupoMusical1 :  GrupoMusical = {
+    nombre: "The Beatles",
+    añoFundacion: 1960,
+    activo: true,
+    genero: popRock,
+}
+
+const grupoMusical2 :  GrupoMusical = {
+    nombre: "Queen",
+    añoFundacion: 1970,
+    activo: false,
+    genero: rock,
+}
+
+const grupoMusical3 :  GrupoMusical = {
+    nombre: "AC DC",
+    añoFundacion: 1973,
+    activo: true,
+    genero: hardRock,
+}
+
+const grupoMusical4 :  GrupoMusical = {
+    nombre: "Ludwig van Beethoven",
+    añoFundacion: 1770,
+    activo: false,
+    genero: clasica,
+}
+
+const grupoMusical5 :  GrupoMusical = {
+    nombre: "The Rolling Stones",
+    añoFundacion: 1962,
+    activo: true,
+    genero: rock,
+}
+
+console.log(grupoMusical1);
+console.log(grupoMusical2);
+console.log(grupoMusical3);
+console.log(grupoMusical4);
+console.log(grupoMusical5);
